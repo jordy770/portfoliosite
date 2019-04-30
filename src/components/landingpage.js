c57bc53c50a3feb530d68099f7828a22f95c1b35
jordy770 <jordy.van.santen@hotmail.com>
2019-04-30 12:53:07 +0200

    uploade

---
+++ src/components/landingpage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './main.css';
+
+class Landing extends Component {
+    render(){
+        return(
+            <div className="intro">
+                hallo
+            </div>
+        )
+    }
+}
+
+export default Landing;
