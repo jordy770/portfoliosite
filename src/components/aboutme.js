a4e5284a5a6591383d4c0c3244b8f36a10af3f54
jordy770 <jordy.van.santen@hotmail.com>
2019-05-09 12:34:48 +0200

    home (Done) begin about page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, ProgressBar } from "react-bootstrap";
 import Typist from "react-typist";
 import "./main.scss";
-
-
 
 class About extends Component {
   render() {
@@ -12,26 +10,106 @@ class About extends Component {
         <Row>
           <Col className="title">
             <Typist
-                className="landing-header"
-                avgTypingSpeed={40}
-                startDelay={2000}
-              >
-                What can i tell about myself             
-                <Typist.Backspace count={28} delay={1000} />
-                <Typist.Delay ms={750} />
-                  About me !
-                <Typist.Delay ms={1250} />
-              </Typist>
+              className="landing-header"
+              avgTypingSpeed={40}
+              startDelay={2000}
+            >
+              What can i tell about myself
+              <Typist.Backspace count={28} delay={1000} />
+              <Typist.Delay ms={750} />
+              About me !
+              <Typist.Delay ms={1250} />
+            </Typist>
           </Col>
         </Row>
 
-
         <Row>
-          <Col className="animated fadeIn part1">
-          <div className="test">Example</div>
-          </Col >
-          <Col className="animated fadeIn part2">
-          
+          <Col sm={12} xl={6} className="animated fadeIn part1">
+            <h1>about</h1>
+            <h3> This is my portfolio website.</h3>
+            <p>
+              {" "}
+              I'm Jordy van Santen and i'm 20 years old <br /> I'm a beginning
+              Creative programmer{" "}
+            </p>
+            <p>
+              {" "}
+              At this moment i'm studying at the university of applied science
+              Rotterdam and the course I'm following is Creative Media and Game
+              Technologies.
+            </p>
+          </Col>
+
+          <Col sm={12} xl={6} className="animated fadeIn part2">
+            <div className="skills">
+              <Container>
+              <Col>
+              <h2>Skills</h2></Col>
+                <Row>
+                <Col>
+                    <div className="items">
+                      <p>Css</p>
+                      <ProgressBar animated now={33} />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div className="items">
+                      <p>Javascript</p>
+                      <ProgressBar variant="warning" animated now={66} />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div className="items">
+                      <p>HTML</p>
+                      <ProgressBar variant="success" animated now={100} />
+                    </div>
+                  </Col>
+                 
+                  
+                </Row>
+                <Row>
+                  <Col>
+                    <div className="items">
+                      <p>woop</p>
+                      <ProgressBar animated now={45} />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div className="items">
+                      <p>woop</p>
+                      <ProgressBar animated now={45} />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div className="items">
+                      <p>woop</p>
+                      <ProgressBar animated now={45} />
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <div className="items">
+                      <p>woop</p>
+                      <ProgressBar animated now={45} />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div className="items">
+                      <p>woop</p>
+                      <ProgressBar animated now={45} />
+                    </div>
+                  </Col>
+                  <Col>
+                    <div className="items">
+                      <p>woop</p>
+                      <ProgressBar animated now={45} />
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+            <div className="-timeline" />
           </Col>
         </Row>
       </Container>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import "./main.scss";
-import {
-  Jumbotron,
-  Container,
-  Row,
-  Col,
-  Image,
-  Button
-} from "react-bootstrap";
+import { Jumbotron, Container, Row, Col, Image, Button } from "react-bootstrap";
 
 import Typist from "react-typist";
 
@@ -17,46 +10,48 @@ class Landing extends Component {
       <Container>
         <Row>
           <Col className="title">
-          <Typist
-                className="landing-header"
-                avgTypingSpeed={40}
-                startDelay={2000}
-              >
-                Welcome to my website              
-                <Typist.Backspace count={7} delay={1000} />
-                <Typist.Delay ms={750} />
-                portfolio
-                <Typist.Delay ms={1250} />
-              </Typist>
+            <Typist
+              className="landing-header"
+              avgTypingSpeed={40}
+              startDelay={2000}
+            >
+              Welcome to my website
+              <Typist.Backspace count={7} delay={1000} />
+              <Typist.Delay ms={750} />
+              portfolio
+              <Typist.Delay ms={1250} />
+            </Typist>
           </Col>
         </Row>
         <Row>
-          <Col sm={12} xl={6} className="intro">
-          <h3> This is my portfolio website.</h3>
-                <p>
-                  {" "}
-                  I'm Jordy van Santen and i'm 20 years old <br /> I'm a
-                  beginning Creative programmer{" "}
-                </p>
-                <p>
-                  {" "}
-                  At this moment i'm studying at the university of applied
-                  science Rotterdam and the course I'm following is Creative
-                  Media and Game Technologies.
-                </p>
-                <p> Click Here if you want to know me Better!!</p>
-
-                <Button className="-button" variant="secondary" size="lg" href="./aboutme">
-                  About ME!
-                </Button>
-                <Button className="-button" variant="secondary" size="lg" href="./projects">
-                  My Projects!
-                </Button>          </Col>
-          <Col sm={12} xl={6} className="pf">
-            <Image src="assets/beter.png" rounded className="profile-pic" />         
+          <Col sm={12} xl={12} className="pf">
+            <Image src="assets/beter.png" rounded className="profile-pic" />
+          </Col>
+          </Row>
+          <br/>
+          <Row>
+          <Col sm={12} xl={12} className="intro">
+           <h5> Hi, This is me Jordy van Santen, I'm a CMGT Student looking for an Internship!
+           </h5>
+            <p> Click Here if you want to know me Better!!</p>
+            <Button
+              className="-button"
+              variant="secondary"
+              size="lg"
+              href="./aboutme"
+            >
+              About ME!
+            </Button>
+            <Button
+              className="-button"
+              variant="secondary"
+              size="lg"
+              href="./projects"
+            >
+              My Projects!
+            </Button>{" "}
           </Col>
         </Row>
-
       </Container>
     );
   }
