1a6662a9e4c02b2a9edc9f714f4734050675e2b5
jordy770 <jordy.van.santen@hotmail.com>
2019-05-18 18:02:37 +0200

    projects update

--- src/components/projects.js
+++ src/components/projects.js
@@ -73,7 +73,7 @@ class Projects extends Component {
         <Draggable bounds="body" handle="strong" {...dragHandlers}>
           <div className="project-box no-cursor">
             <strong className="cursor">
-              <div>GameSite</div>
+              <div>API & GameSite</div>
             </strong>
             <Image className="project-pic" src="assets/gamesite.png" />
             <Row>
@@ -103,7 +103,7 @@ class Projects extends Component {
             <strong className="cursor">
               <div>ThatShot</div>
             </strong>
-            <Image className="project-pic" src="assets/beter.png" />
+            <Image className="project-pic" src="assets/thatshot.png" />
             <Row>
               <Col
                 md={{ span: 10, offset: 1 }}
@@ -131,7 +131,63 @@ class Projects extends Component {
             <strong className="cursor">
               <div>Chappe</div>
             </strong>
-            <Image className="project-pic" src="assets/beter.png" />
+            <Image className="project-pic" src="assets/chappe.png" />
+            <Row>
+              <Col
+                md={{ span: 10, offset: 1 }}
+                className=" d-flex align-items-center"
+              >
+                <div className="project-info">
+                  They were dropping, losing altitude in a canyon of rainbow
+                  foliage, a lurid communal mural that completely covered the
+                  hull of the bright void beyond the chain link. Still it was a
+                  handgun and nine rounds of ammunition
+                </div>
+              </Col>
+            </Row>
+            <br />
+            <div className="project-button">
+              <Button variant="secondary" size="lg" href="./1#">
+                Click here!
+              </Button>
+            </div>
+          </div>
+        </Draggable>
+
+        <Draggable bounds="body" defaultPosition={{x: 100, y: -100}} handle="strong" {...dragHandlers}>
+          <div className="project-box no-cursor">
+            <strong className="cursor">
+              <div>Portfolio V1</div>
+            </strong>
+            <Image className="project-pic" src="assets/portfolio-v1.png" />
+            <Row>
+              <Col
+                md={{ span: 10, offset: 1 }}
+                className=" d-flex align-items-center"
+              >
+                <div className="project-info">
+                  They were dropping, losing altitude in a canyon of rainbow
+                  foliage, a lurid communal mural that completely covered the
+                  hull of the bright void beyond the chain link. Still it was a
+                  handgun and nine rounds of ammunition
+                </div>
+              </Col>
+            </Row>
+            <br />
+            <div className="project-button">
+              <Button variant="secondary" size="lg" href="./1#">
+                Click here!
+              </Button>
+            </div>
+          </div>
+        </Draggable>
+
+        <Draggable bounds="body" defaultPosition={{x: 100, y: -100}} handle="strong" {...dragHandlers}>
+          <div className="project-box no-cursor">
+            <strong className="cursor">
+              <div>Portfolio V2</div>
+            </strong>
+            <Image className="project-pic" src="assets/portfolio-v2.png" />
             <Row>
               <Col
                 md={{ span: 10, offset: 1 }}
